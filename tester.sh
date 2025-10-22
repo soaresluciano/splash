6caa43d99c177ae85d5ccddb2a312ac137483b77
Luciano Soares <1084292+soaresluciano@users.noreply.github.com>
2025-10-22 16:25:35 +0200

    fix

--- tester.sh
+++ tester.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source ./spla.sh
 
-# recommended: call the function directly using run_autoinput
-run_output_contains out "Operation cancelled by user." run_autoinput 'n' prompt_proceed "RESOURCE"
+run_autoinput "n" prompt_overwrite "RESOURCE"
 echo $?
